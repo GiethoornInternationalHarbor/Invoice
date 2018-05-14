dedfcd3200c41521b73eb0153b517bc370f09558
Kevin Ly <kly1@avans.nl>
2018-05-14 20:37:16 +0200

    Init infrastructure, Created event handlers -> not all implemented.

---
+++ InvoiceService.App/Messaging/InvoiceMessageHandler.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Threading.Tasks;
+using InvoiceService.Core.Messaging;
+using InvoiceService.Core.Models;
+using InvoiceService.Core.Repositories;
+using Utf8Json;
+
+namespace InvoiceService.App.Messaging
+{
+	public class InvoiceMessageHandler : IMessageHandlerCallback
+	{
+		private readonly IInvoiceRepository _invoiceRepository;
+		private readonly ICustomerRepository _customerRepository;
+		private readonly IShipRepository _shipRepository;
+		private readonly IMessagePublisher _messagePublisher;
+
+		public InvoiceMessageHandler(IInvoiceRepository invoiceRepository, ICustomerRepository customerRepository, IShipRepository shipRepository, IMessagePublisher messagePublisher)
+		{
+			_invoiceRepository = invoiceRepository;
+			_customerRepository = customerRepository;
+			_shipRepository = shipRepository;
+			_messagePublisher = messagePublisher;
+		}
+
+		public async Task<bool> HandleMessageAsync(MessageTypes messageType, string message)
+		{
+			switch (messageType)
+			{
+				case MessageTypes.CustomedUpdated:
+					{
+						return await HandleCustomerUpdated(message);
+					}
+				case MessageTypes.CustomerCreated:
+					{
+						return await HandleCustomerCreated(message);
+					}
+				case MessageTypes.RentalAccepted:
+					{
+						return await HandleRentalAccepted(message);
+					}
+				case MessageTypes.RentalRequested:
+					{
+						return await HandleRentalRequested(message);
+					}
+				case MessageTypes.ServiceCompleted:
+					{
+						return await HandleServiceCompleted(message);
+					}
+				case MessageTypes.ServiceCreated:
+					{
+						return await HandleServiceCreated(message);
+					}
+				case MessageTypes.ServiceDeleted:
+					{
+						return await HandleServiceDeleted(message);
+					}
+				case MessageTypes.ServiceRequested:
+					{
+						return await HandleServiceRequested(message);
+					}
+				case MessageTypes.ServiceUpdated:
+					{
+						return await HandleServiceUpdated(message);
+					}
+				case MessageTypes.ShipDocked:
+					{
+						return await HandleShipDocked(message);
+					}
+				case MessageTypes.ShipUndocked:
+					{
+						return await HandleShipUndocked(message);
+					}
+			}
+
+			return true;
+		}
+
+		private async Task<bool> HandleCustomerUpdated(string message)
+		{
+			var receivedCustomer = JsonSerializer.Deserialize<Customer>(message);
+
+			await _customerRepository.UpdateCustomerAsync(receivedCustomer);
+
+			return true;
+		}
+
+		private async Task<bool> HandleCustomerCreated(string message)
+		{
+			var receivedCustomer = JsonSerializer.Deserialize<Customer>(message);
+
+			await _customerRepository.CreateCustomerAsync(receivedCustomer);
+
+			return true;
+		}
+
+		private async Task<bool> HandleRentalAccepted(string message)
+		{
+
+			return true;
+		}
+
+		private async Task<bool> HandleRentalRequested(string message)
+		{
+
+			return true;
+		}
+
+		private async Task<bool> HandleServiceCompleted(string message)
+		{
+
+
+			return true;
+		}
+
+		private async Task<bool> HandleServiceCreated(string message)
+		{
+
+
+			return true;
+		}
+
+		private async Task<bool> HandleServiceDeleted(string message)
+		{
+
+			return true;
+		}
+
+		private async Task<bool> HandleServiceRequested(string message)
+		{
+
+			return true;
+		}
+
+		private async Task<bool> HandleServiceUpdated(string message)
+		{
+
+			return true;
+		}
+
+		private async Task<bool> HandleShipDocked(string message)
+		{
+			var receivedShip = JsonSerializer.Deserialize<Ship>(message);
+
+			await _shipRepository.CreateShip(receivedShip);
+
+			return true;
+		}
+
+		private async Task<bool> HandleShipUndocked(string message)
+		{									  
+			var receivedShip = JsonSerializer.Deserialize<Ship>(message);	
+
+			var invoice = await _invoiceRepository.GetInvoiceAsync(receivedShip.Id);
+
+			await _messagePublisher.PublishMessageAsync(MessageTypes.InvoiceCreated, invoice);
+
+			await _shipRepository.DeleteShip(receivedShip.Id);
+
+			return true;
+		}
+	}
+}
---
+++ InvoiceService.Infrastructure/DI/DIHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace InvoiceService.Infrastructure.DI
+{
+	public static class DIHelper
+    {
+		public static void Setup(IServiceCollection services, IConfiguration configuration)
+		{
+			
+		}
+
+		public static void OnServicesSetup(IServiceProvider serviceProvider)
+		{
+									 
+
+		} 
+	}
+}
