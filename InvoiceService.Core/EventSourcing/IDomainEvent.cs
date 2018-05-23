8071af9d64f9b0e86c9c14e545a499e06cbe2bb7
Kevin Ly <kly1@avans.nl>
2018-05-23 11:15:00 +0200

    added EventSourcing

---
+++ InvoiceService.Core/EventSourcing/IDomainEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InvoiceService.Core.EventSourcing
+{
+	public interface IDomainEvent<TAggregateId>
+	{
+		Guid EventId { get; }
+
+		TAggregateId AggregateId { get; }
+
+		long AggregateVersion { get; }
+	}
+}
