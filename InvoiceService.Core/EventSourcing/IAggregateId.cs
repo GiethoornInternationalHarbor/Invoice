ba679fbaec8515b5238eb44e04357f90a0aad3c0
Marco Havermans <marcohavermans+github@hotmail.nl>
2018-05-23 11:28:09 +0200

    Added interface / classes for event sourcing

---
+++ InvoiceService.Core/EventSourcing/IAggregateId.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InvoiceService.Core.EventSourcing
+{
+	public interface IAggregateId
+	{
+		string IdAsString();
+	}
+}
---
+++ InvoiceService.Infrastructure/EventSourcing/EventSourcingRepository.cs
@@ -0,0 +1,53 @@
+﻿using InvoiceService.Core.EventSourcing;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceService.Infrastructure.EventSourcing
+{
+	public class EventSourcingRepository<TAggregate, TAggregateId> : IEventSourcingRepository<TAggregate, TAggregateId>
+	  where TAggregate : AggregateBase<TAggregateId>, IAggregate<TAggregateId>
+	  where TAggregateId : IAggregateId
+	{
+		private readonly IEventStore eventStore;
+
+		public EventSourcingRepository(IEventStore eventStore)
+		{
+			this.eventStore = eventStore;
+		}
+
+		public async Task<TAggregate> GetByIdAsync(TAggregateId id)
+		{
+			var aggregate = CreateEmptyAggregate();
+			IEventSourcingAggregate<TAggregateId> aggregatePersistence = aggregate;
+
+			foreach (var @event in await eventStore.ReadEventsAsync(id))
+			{
+				aggregatePersistence.ApplyEvent(@event.DomainEvent, @event.EventNumber);
+			}
+			return aggregate;
+		}
+
+		public async Task SaveAsync(TAggregate aggregate)
+		{
+			IEventSourcingAggregate<TAggregateId> aggregatePersistence = aggregate;
+
+			foreach (var @event in aggregatePersistence.GetUncommittedEvents())
+			{
+				await eventStore.AppendEventAsync(@event);
+			}
+			aggregatePersistence.ClearUncommittedEvents();
+		}
+
+		private TAggregate CreateEmptyAggregate()
+		{
+			return (TAggregate)typeof(TAggregate)
+			  .GetConstructor(
+				BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public,
+				null, new Type[0], new ParameterModifier[0])
+			  .Invoke(new object[0]);
+		}
+	}
+}
